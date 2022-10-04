c7150ac112a5894f41eeb3d99b521232cd120feb
SimonielMusyoki <musyoki.mtk3@gmail.com>
2022-10-04 05:09:57 +0300

    Project Cleanup

--- blog/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Messages
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Messages
-        fields = ['name','email','message']
--- properties/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Properties
-class PropertiesForm(forms.ModelForm):
-    class Meta:
-        model = Properties
-        fields = ['PropertyName', 'Address', 'City', 'County','Picture1',]
