611be71fe0efa64a0874514ea31cfb29968e7954
SimonielMusyoki <musyoki.mtk3@gmail.com>
2022-10-23 10:37:49 +0300

    Chore: Set up Nginx with react & Docker

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello React</h2>
+      <p>Here is the front end UI for the Real Estate API</p>
+    </div>
+  );
+}
+
+export default App;
