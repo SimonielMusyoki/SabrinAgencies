bee560d3abeec73770461c6d349bb13a9e819782
SimonielMusyoki <musyoki.mtk3@gmail.com>
2022-10-29 10:02:46 +0300

    feat(client): Add property page

--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-
+import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
+import Property from "../components/Property";
 import { getProperties, reset } from "../features/properties/propertyslice";
 
 const PropertiesPage = () => {
-  const { properties, isLoading, isSuccess } = useSelector(
+  const { properties, isLoading, isError, message } = useSelector(
     (state) => state.properties
   );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
     dispatch(getProperties());
-  }, [dispatch]);
+  }, [dispatch, isError, message]);
 
   if (isLoading) {
     return <Spinner />;
@@ -23,10 +27,22 @@ const PropertiesPage = () => {
     <>
       <Container>
         <Row>
-          <Col className="mg-top">
-            <h1>Properties will be displayed here</h1>
+          <Col className="mg-top text-center">
+            <h1>Our Properties Catalog</h1>
+            <hr className="hr-text" />
           </Col>
         </Row>
+        {
+          <>
+            <Row className="mt-3">
+              {properties.map((property) => (
+                <Col key={property.id} sm={12} md={6} lg={4} xl={3}>
+                  <Property property={property} />
+                </Col>
+              ))}
+            </Row>
+          </>
+        }
       </Container>
     </>
   );
